3d7016f0451503001416b5c84c47bc43fa526f25
fwojtowicz <fil.wojtowicz@gmail.com>
2018-10-23 19:20:43 +0200

    working CRUD with ISBN validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ApplicationRecord
-    validates :isbn, length: {minimum: 10, maximum: 13 }
+    validates :isbn, length: {minimum: 10, maximum: 13 }, format: {with: /\d/, message: "numbers only"}
 end
