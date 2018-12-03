f3c7dc236ff7ba6595402c95f50a8acb3a233bae
Filip Wójtowicz <fil.wojtowicz@gmail.com>
2018-12-03 14:41:42 +0100

    added navigation links, skipped fooling around with bootstrap, fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   resources :books
-<<<<<<< HEAD
   get 'books/index'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-=======
-  root 'static_pages#home'
-  get 'books/index'
->>>>>>> filling-in-layout
 end
