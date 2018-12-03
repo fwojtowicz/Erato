ba6406996ef48cde251ee9a81e64e242164c46f8
Filip Wójtowicz <fil.wojtowicz@gmail.com>
2018-12-03 14:12:19 +0100

    added navigation links, skipped fooling around with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :books
-  root 'books#index'
+  root 'static_pages#home'
+  get 'books/index'
 end
