e9e238f67762f1fcf9f314bbb469b1e503667632
Filip Wójtowicz <fil.wojtowicz@gmail.com>
2019-11-23 11:24:39 +0100

    enable CORS for netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://eurekabooks.netlify.com'
+      origins 'https://eurekabooks.netlify.com', 'http://localhost:8080'
       resource '*',
         headers: :any,
         methods: %i(get post put patch delete options head)
